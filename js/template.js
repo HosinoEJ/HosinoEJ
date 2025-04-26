956d0f770426868ba28ad4113225d2904dbe3b5f
星野栄治 <hosinoeiji@gmail.com>
2025-04-26 22:39:41 +0800

    beta0.9更新
    添加了按鈕的動畫效果
    添加了自己紹介的按鈕
    維護了“指令集”功能
    宣佈網站使用GUN協議
    添加了自己紹介的頁面（未完工）
    刪除了測試頁面

--- js/template.js
+++ js/template.js
@@ -1,35 +1,30 @@
-// 加载模板
 fetch('../template.html')
   .then(response => response.text())
   .then(data => {
     const tempDiv = document.createElement('div');
     tempDiv.innerHTML = data;
 
-    // 缓存所有模板内容（原始内容，不clone）
+    // 缓存所有模板内容
     const templates = new Map();
     tempDiv.querySelectorAll('template').forEach(template => {
-      templates.set(template.id, template.content);  // 不 clone
+      templates.set(template.id, template.content.cloneNode(true));
     });
 
     // 遍历页面中的元素并插入对应的模板
     templates.forEach((content, id) => {
-      document.querySelectorAll(`.${id}`).forEach(el => {
-        if (el.children.length === 0) {  // 防止重复插入
-          el.appendChild(content.cloneNode(true));  // 这里再 clone
-        }
-      });
+      document.querySelectorAll(`.${id}`).forEach(el => 
+        el.appendChild(content.cloneNode(true))  // 克隆缓存的内容
+      );
     });
   });
-
-
-(function () {
-  const originalLog = console.log;
-  window.hsnej = function () {window.location.href = 'hsnej_cd.html';};
-  window.info = function () {alert("星野栄治 © 2025｜GNU LICENSEによって転載してください");
-  };
-  console.log = function (message) { originalLog.apply(console, arguments);
-  };
-
-})(
-
-);
+  (function () {
+    const originalLog = console.log;
+    window.hsnej = function () {window.location.href = 'hsnej_cd.html';};
+    window.info = function () {alert("星野栄治 © 2025｜GNU LICENSEによって転載してください");
+    };
+    console.log = function (message) { originalLog.apply(console, arguments);
+    };
+  
+  })(
+  
+  );
